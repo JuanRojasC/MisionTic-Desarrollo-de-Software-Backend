f50e727aaa74d92e65201a5fb510dd1dddb66980
IvánDueñas <ivan.duenas.mt@correo.usa.edu.co>
2021-11-11 09:17:24 -0500

    Se completa clase entidad partyroom

--- src/main/java/com/misiontic/backend_desarrollo_de_software/controller/SalonFiestaController.java
+++ src/main/java/com/misiontic/backend_desarrollo_de_software/controller/SalonFiestaController.java
@@ -1,4 +1,34 @@
 package com.misiontic.backend_desarrollo_de_software.controller;
 
+import com.misiontic.backend_desarrollo_de_software.model.SalonFiesta;
+import com.misiontic.backend_desarrollo_de_software.service.SalonFiestaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/partyroom")
 public class SalonFiestaController {
+    
+   private SalonFiestaService salonFiestaService;
+   
+   @Autowired
+   public SalonFiestaController(SalonFiestaService salonFiestaService) {
+        this.salonFiestaService = salonFiestaService;
+    }
+   
+   @GetMapping("/all")
+   public ResponseEntity<?> buscarTodosLosSalones(){
+        //return ResponseEntity.status(HttpStatus.OK).body(salonFiestaService.buscarTodosLosSalones());
+        return ResponseEntity.ok(salonFiestaService.buscarTodosLosSalones());
+    }
+    
+   @PostMapping("/save")
+    public ResponseEntity<?> guardarSalonFiesta(@RequestBody SalonFiesta c){
+        return ResponseEntity.ok(salonFiestaService.guardarSalonFiesta(c));
+    }
 }
